fdf4f568f3ffbe094de25d830d604feef28166df
Oluwatosin Folaranmi <foluwatosin@ITILP_L014.simplexsystems.com>
2019-02-05 14:06:55 +0100

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  cart = [];
+  items = [];
+
+  sliderConfig = {
+    slidersPerView: 1.6,
+    spaceBetween:10,
+    centeredClides: true
+  };
+
+  constructor(private router: Router, private cartService: CartService){ }
+
+  ngOnInit() {
+    this.items = this.cartService.getProducts();
+    this.cart = this.cartService.getCart  ();
+  }
+
+  addToCart(product) {
+    this.cartService.addProduct(product);
+  }
+
+  openCart() {
+    this.router.navigate(['cart']);
+  }
 
 }
